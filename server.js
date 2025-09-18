d43da90415a6c495242dc4bf2c6059917d374e97
Tạ Minh Dũng <136634915+taminhdung@users.noreply.github.com>
2025-09-18 12:02:40 +0700

    update server

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const loginUser = require('./service/login'); // import hàm đăng nhập user
 import cors from "cors";
 
 const app = express();// khởi tạo Express app
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json()); // Cho phép đọc JSON từ body
 
 const options = {//tạo chứng chỉ SSL
